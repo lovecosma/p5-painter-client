6152538c90079184f2b232afcfafe448ac588945
lovecosma <williamsmatthew333@gmail.com>
2020-11-18 12:08:03 -0500

    continued adjusting granular synthesis for sound_particle objects

--- painter/sketch.js
+++ painter/sketch.js
@@ -4,6 +4,7 @@ let vScale = 8
 let buffers
 let particles = [];
 let sound_particles = []
+let slider;
 
 let loaded = false
 
@@ -14,35 +15,25 @@ function preload(){
 
 
 function setup() {
-    // use parent to render the canvas in this ref
-    // (without that p5 will render the canvas outside of your component)
     createCanvas(640, 480);
     pixelDensity(1)
     video = createCapture(VIDEO)
     video.size(width/vScale, height/vScale)
     for(let i = 0; i < 4; i++){
-        sound_particles.push(new SoundParticle(random(0, width), random(0, height), i))
+        sound_particles.push(new SoundParticle(random(0, width), random(0, height), random(0, Object.entries(buffers).length -1)))
     }
     for(let i = 0; i < 100; i++){
         particles.push(new Particle(random(0, width), random(0, height)))
     }
+    slider = createSlider(0, 255, 100, 1);
+    slider.position(0, 670);
+    slider.style('width', '80px');
 };
 
 function draw(){
     video.loadPixels();
-    // sound_particle.show()
-    // sound_particle.update()
-    // for(let i = 0; i < particles.length; i++){
-    //     particles[i].update();
-    //     particles[i].show();
-    // }
     for(let i = 0; i < sound_particles.length; i++){
         sound_particles[i].update();
         sound_particles[i].show();
     }
-
-
-    // NOTE: Do not use setState in the draw function or in functions that are executed
-    // in the draw function...
-    // please use normal variables or class properties for these purposes
 };
--- painter/sound_particle.js
+++ painter/sound_particle.js
@@ -1,7 +1,7 @@
 function SoundParticle(x, y, i){
     this.x = x;
     this.y = y;
-    this.i = i;
+    this.i = floor(i);
     this.loaded = false
     this.grainVol = 0
     this.grainOverlap = 0.05
@@ -14,11 +14,11 @@ function SoundParticle(x, y, i){
         "volume": this.grainVol,
         "overlap": this.grainOverlap,
         "grainSize": this.grainSize,
-        "playbackRate": 0.5,
+        "playbackRate": this.grainPlaybackRate,
         "detune": this.grainDetune,
         "loop": true,
-        "loopStart": 0.5,
-        "loopEnd": 1,
+        "loopStart": 0,
+        "loopEnd": 4,
         "reverse": false
     })
     this.grain.toDestination().start()
@@ -31,36 +31,31 @@ function SoundParticle(x, y, i){
         } else if(this.x < 0){
             this.x = 640
             this.grain.reverse = !this.grain.reverse
-            this.grain.mute = !this.grain.mute
         }
         if(this.y > 420){
             this.y = 0
-            this.grain.playbackRate = this.grain.playbackRate * 2
+            // this.grain.playbackRate = this.grain.playbackRate * 2
 
         } else if(this.y < 0){
            this.y = 420
-           this.grain.playbackRate = this.grain.playbackRate * 0.5
+        //    this.grain.playbackRate = this.grain.playbackRate * 0.5
         }
     }
 
     this.show = function(){
         noStroke();
+        let val = slider.value()
         let px = floor(this.x / vScale)
         let py = floor(this.y / vScale)
         let col = video.get(px, py)
-        let avg = ((col[0]+col[1]+col[2])/3)
+        this.grain.volume.value = map(col[0], 0, 255, -30, 5)
         this.grainOverlap = map(col[2], 0, 255, 0.1, 0.9)
         this.grain.grainSize = map(col[1], 0, 255, 0.1, 0.9)
-        this.grain.detune = map(col[0], 0, 255, -1200, 1200)
-        if(round(this.x % 3) == 0){
-            this.grain.mute =true
-        } else {
-            this.grain.mute =false
-        }
-        console.log(round(this.x % 3) == 0);
-        let volume = map(col[2], 0, 255, -1, 10)
-        this.grain.volume.value = volume
-        fill(col[0], col[1], col[2])
+        this.grain.detune = map(col[0], 0, 255, -2400, 2400)
+      
+
+        fill(col[0], col[1], col[2], val)
+
         ellipse(this.x, this.y, 24, 24);
     }
 
